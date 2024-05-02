7c32d0902aca1feae7e56cdf28b5d7f3aa067966
sebaek <sebaek@gmail.com>
2024-05-02 09:49:00 +0900

    회원가입 form

--- src/main/java/com/prj1/controller/MemberController.java
+++ src/main/java/com/prj1/controller/MemberController.java
@@ -1,4 +1,21 @@
 package com.prj1.controller;
 
+import com.prj1.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("member")
 public class MemberController {
+
+    private final MemberService service;
+
+    @GetMapping("signup")
+    public String signupForm() {
+
+        return "member/signup";
+    }
 }
---
+++ src/main/java/com/prj1/service/MemberService.java
@@ -0,0 +1,14 @@
+package com.prj1.service;
+
+import com.prj1.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberMapper mapper;
+}
