016f88d6e0cdf736a5073c7ec6a2d8f801567727
sebaek <sebaek@gmail.com>
2024-05-07 10:04:44 +0900

    권한

--- src/main/java/com/prj1/domain/CustomUser.java
+++ src/main/java/com/prj1/domain/CustomUser.java
@@ -1,9 +1,8 @@
 package com.prj1.domain;
 
 import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-
-import java.util.List;
 
 
 @Getter
@@ -11,10 +10,12 @@ public class CustomUser extends User {
     private Member member;
 
     public CustomUser(Member member) {
-        super(member.getEmail(), member.getPassword(), List.of());
-        if (member.getAuthority() == null) {
+        super(member.getEmail(),
+                member.getPassword(),
+                member.getAuthority().stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .toList());
 
-        }
         this.member = member;
     }
 }
